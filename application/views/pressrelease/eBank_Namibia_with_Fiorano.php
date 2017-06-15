aee7e3dfbd2fb92c9314cd036fc294af9a34f242
harikrishnan <harikrishnan.v@in.fiorano.com>
2017-06-15 14:55:32 +0530

    Updates From Hari 15-06-2017

---
+++ application/views/pressrelease/eBank_Namibia_with_Fiorano.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harikrishnan
+ * Date: 6/14/2017
+ * Time: 1:07 PM
+ */?>
+
+<section  class="pressrelease-banner">
+    <div class="container">
+        <div class="col-xs-12  text-center padding-top-20">
+            <h2>Press Releases</h2>
+        </div>
+    </div>
+    <a href="#"> &nbsp;</a>
+    <div class="bs-example pull-right padding-top-50">
+        <ul class="breadcrumb">
+            <li> <a href="<?php echo site_url(''); ?>">Home</a></li>
+            <li> <a href="<?php echo site_url('company/pressreleases'); ?>">Press Releases</a></li>
+            <li class="active">EBank Namibia </li>
+        </ul>
+    </div>
+</section>
+
+<section class="pressrelease-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 padding-bottom-20 padding-top-20">
+                <h3>
+                    EBank Namibia facilitates digital banking with Fiorano</br>
+                    Fiorano Enterprise Service Bus seamlessly integrates EBank’s Temenos T24 core banking system with 3rd party suppliers and channels
+                </h3>
+            </div>
+
+            <div class="col-xs-12 pressrelease-uservoice padding-top-20 padding-bottom-20">
+                <div class=" col-sm-2"><img class="img-responsive" src="<?php echo base_url('assets/images/pressreleases/eBank.jpg')?>"></div>
+                <div class=" col-sm-8">"We at EBank are impressed with the flexibility of the Fiorano ESB, Enterprise Service Bus. The application is user friendly, highly agile and enables EBank to quickly and securely set up connections with third party systems. The Fiorano ESB consumes a fraction of the resources normally required by similar applications thus mitigating the need to heavily invest in server infrastructure."</div>
+                <div class=" col-sm-2 padding-top-20"> <strong>Gerald Riedel<br>
+                        CFO, EBank</strong></div>
+
+            </div>
+            <div class="col-xs-12">
+                <div class="col-sm-9 padding-top-50 padding-bottom-50">
+                    <p>
+                        June 20, 2017, London – Fiorano Software, a leader in enterprise integration, today announced that EBank, a major commercial bank operating in Namibia, has successfully implemented Fiorano ESB to fulfill integration requirements at the Bank. The EBank team was specifically looking for an easy to configure middleware solution to handle a host of different interfaces with the ability to be operated without onsite programmers. With its out-of-the-box integration with Temenos T24 core banking, the Fiorano ESB perfectly suited their requirements and allowed the EBank team to swiftly set up its interfaces for various channels.
+                    </p><p>
+                        EBank’s vision is to empower customers with branchless banking, radically increasing convenience for its customers. EBank has achieved digital banking transformation through user friendly electronic channels that enable clients to open accounts, link debit cards and transact from their mobile phone or personal computer without having to walk into a physical branch. In addition, EBank has associations with a network of retail outlets at which EBank clients can withdraw and deposit cash and pay for goods or services at the retailer using just their mobile phones.
+
+                    </p><p>
+                        The integrations with retail partners and other value-added services provider are complex in nature and require powerful applications to ensure that transactions flowing through these integrations are transmitted in real-time and secure. <a href="<?php echo site_url('products/fiorano_esb')?>" target="_blank"> Fiorano ESB </a>, underpinned with its unique peer-to-peer architecture and complimented with a pre-built library of  <a href="<?php echo site_url('products/fiorano_microservices')?>" target="_blank"> Microservices</a>, empowers the bank to rapidly onboard partners across multiple delivery channels. Gerald Riedel of EBank further commented,”….in addition, Fiorano’s clients, such as banks have the option to buy a multitude of different microservices/adapters that convert most known message standards or message types from one format into another. This includes various ISO, xml & text adaptors. As such, customization at core banking system level is minimal as all message format conversion is handled efficiently by ESB.”
+                    </p>
+
+                    <h3>About EBank</h3>
+
+                    <p>EBank is the 7th Commercial Bank in Namibia and was launched by the Pointbreak Group. The Pointbreak Group, including EBank, were recently acquired by FNB Namibia Holdings. EBank and FNB have a similar vision of achieving inclusive and broad-based banking in Namibia, primarily utilizing technology and mobile phone banking capabilities. The partnership will eventually enable the activation of FNB’s eWallet to EBank’s client base.
+                    </p>
+                    <h3>About Fiorano</h3>
+                    <p>
+                        Founded in 1995, Fiorano is a trusted provider of enterprise integration middleware, high performance messaging and distributed systems. Fiorano's innovative event-driven SOA platform integrates applications and complex technologies into an enterprise nervous system. Fiorano's hybrid Cloud-ESB integrates applications across SaaS, PaaS and on-premise worlds.
+                    </p><p>
+                        Global leaders including Federal Bank, Commercial Bank of Africa, NASA, US Coast Guard, POSCO, British Telecom, AT&T Labs, Vodafone and L'Oréal have deployed Fiorano's solutions to drive innovation through open, standards-based, event-driven SOA applications yielding unprecedented productivity.
+                    </p>
+                </div>
+                <div class="col-sm-3 nopadding">
+                    <div class="col-xs-12  padding-top-50  shareBoxPress">
+
+                        <div class="col-xs-12 nopadding text-center">
+                            <h4>Share this Press release</h4>
+                            <hr class="double-white">
+                        </div>
+                        <div class="col-xs-12 nopadding">
+                            <div class="social-btns nopadding  ">
+
+
+
+
+
+
+                                <a class="share-btn btn facebook" href="https://www.facebook.com/sharer/sharer.php?app_id=728682737333595&sdk=joey&u=www.fiorano.com&display=popup&ref=plugin&src=share_button" onclick="return !window.open(this.href, 'Facebook', 'width=640,height=580')">
+                                <a class="btn twitter" href="#"><i class="fa fa-twitter"></i></a>
+                                <a class="btn google" href="#"><i class="fa fa-google"></i></a>
+                                <a class="btn dribbble" href="#"><i class="fa fa-envelope"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+
+
+
+        <div class="col-xs-12 text-center">
+            <img src="<?php echo base_url('assets/images/company/press-release-bottom-image.jpg') ?>" class="img-responsiveq"/>
+        </div>
+    </div>
+</section>
