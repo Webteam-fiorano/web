6db7e6e503a13911907be0e7960e9f91c4b94a10
harikrishnan <harikrishnan.v@in.fiorano.com>
2017-06-15 14:54:58 +0530

    Updates From Hari 14-06-2017

--- application/controllers/Pressreleases.php
+++ application/controllers/Pressreleases.php
@@ -20,5 +20,11 @@ class Pressreleases extends CI_Controller
      $this->load->view('pressrelease/cpb_fiorano_for_temenos_t24_integration');
      $this->load->view('common/footer');
 }
-
+    public function eBank_Namibia_with_Fiorano(){
+        $data['heading']="Fiorano Enterprise Service Bus seamlessly integrates EBank’s Temenos T24 core banking system with 3rd party suppliers and channels | Fiorano";
+        $data['title']="Fiorano Enterprise Service Bus seamlessly integrates EBank’s Temenos T24 core banking system with 3rd party suppliers and channels | Fiorano";
+        $this->load->view('common/header',$data);
+        $this->load->view('pressrelease/eBank_Namibia_with_Fiorano');
+        $this->load->view('common/footer');
+    }
 }
